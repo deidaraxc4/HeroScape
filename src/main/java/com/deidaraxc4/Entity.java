2dd22e366bbf7059c696acdb21a9f792dfc019bf
Nick Tang <nicktang@s172-21-163-h44.paws.uga.edu>
2018-03-01 14:22:12 -0500

    created entity parent class

---
+++ src/main/java/com/deidaraxc4/Entity.java
@@ -0,0 +1,55 @@
+
+public class Entity {
+    private int health;
+    private int strength;
+    private int mana;
+    
+    /**
+     * Default constructor
+     */
+    public Entity() {
+	this.health = 0;
+	this.strength = 0;
+	this.mana = 0;
+    } // Entity
+    
+    /**
+     * Checks status of health
+     * @return true if health greater than 0
+     */
+    public boolean isAlive() {
+	if(health>0) return true;
+	
+	return false;
+    } // isAlive
+
+
+    // Getters Setters
+
+    public int getHealth() {
+	return this.health;
+    } // getHealth
+
+    public int getStrength() {
+	return this.strength;
+    } // getStrength
+
+    public int getMana() {
+	return this.mana;
+    } // getMana
+
+    public void setHealth(int n) {
+	this.health += n;
+    } // setHealth
+
+    public void setStrength(int n) {
+	this.strength += n;
+    } // setStrength
+
+    public void setMana(int n) {
+	this.mana += n;
+    } // setMana
+
+    
+
+} // Entity
