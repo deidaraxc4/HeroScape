f51e82143b1d10245a26ccea2ec46b00f3b90044
Nick Tang <nicktang@s172-21-119-h146.paws.uga.edu>
2018-03-02 11:27:22 -0500

    added skeleton for first champion

---
+++ src/main/java/com/deidaraxc4/Saber.java
@@ -0,0 +1,20 @@
+package com.deidaraxc4;
+
+public class Saber extends Entity implements Melee {
+    
+    /**
+     * Default constructor
+     */
+    public Saber() {
+
+    } // Saber
+
+    public int basicAttack() {
+	return 0;
+    } // basicAttack
+
+    public int getType() {
+	return 0;
+    } // getType
+
+} // Saber
